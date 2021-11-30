397336ac97a6addbeb7566d264a90f860ae51f4a
Mina Bavosi <minabavo@hotmail.it>
2021-11-30 11:44:01 +0100

    update

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,19 @@
 let clientSocket = io();
+let myColor = "black";
+let background = [];
 
 clientSocket.on("connect", newConnection);
-clientSocket.on("mpuseBroadcast", newBroadcast);
+clientSocket.on("mouseBroadcast", newBroadcast);
+clientSocket.on("color", setColor);
+clientSocket.on("index", setIndex);
+
+function setColor(assignedColor) {
+  myColor = assignedColor;
+}
+
+function setIndex(assignedIndex) {
+  myIndex = assignedIndex;
+}
 
 function newConnection() {
   console.log(clientSocket.id);
@@ -13,17 +25,37 @@ function newBroadcast(data) {
   circle(data.x, data.y, 10);
 }
 
+function drawOtherMouse(data) {
+  push();
+  noStroke();
+  fill(255, 255, 255, 63);
+  ellipse(data.x, data.y, 20);
+  pop();
+}
+
+function preload() {
+  img = loadImage("bagni.jpg");
+  background.push(img);
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(220);
+  background(background[myIndex]);
+  console.log(myIndex);
+  textSize(30);
+  textAlign(CENTER, CENTER);
+  text("a message for your crush", windowWidth / 2, windowHeight / 2);
 }
 
-function draw() {
-  fill("yellow");
-  circle(mouseX, mouseY, 20);
-}
+function draw() {}
 
 function mouseMoved() {
+  push();
+  noStroke();
+  fill(myColor);
+  tint(255, 126);
+  ellipse(mouseX, mouseY, 20);
+  pop();
   let message = {
     x: mouseX,
     y: mouseY,
--- server.js
+++ server.js
@@ -20,12 +20,40 @@ io.on("connection", newConnection);
 
 function newConnection(newSocket) {
   console.log(newSocket.id);
-
   newSocket.on("mouse", mouseMessage);
+
+  let clientColor = getRandomColor();
+  socket.emit("color", clientColor);
+
+  let clientIndex = getRandomIndex();
+  socket.emit("index", clientIndex);
 
   function mouseMessage(dataReceived) {
     console.log(dataReceived);
 
     newSocket.broadcast.emit("mouseBroadcast", dataReceived);
   }
+}
+
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var greys = [
+    "#303030",
+    "#505050",
+    "#696969",
+    "#808080",
+    "#989898",
+    "#A9A9A9",
+    "#C0C0C0",
+    "#D3D3D3",
+    "#F5F5F5",
+  ];
+  var color = greys[Math.floor(Math.random() * 9)];
+  return color;
+}
+
+function getRandomIndex() {
+  var index = Math.floor(Math.random() * 6);
+  console.log(index);
+  return index;
 }
